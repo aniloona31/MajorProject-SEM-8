ab7a5439d37bcbde0c38cdec77df1445173db9ef
anirudh loona <anirudhloona26@gmail.com>
2024-01-16 15:58:02 +0530

    added service registry for other services

---
+++ service-registry/sem8/src/test/java/com/major/sem8/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.major.sem8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
