8a27ee414ecea01f47afe8e649df62d663b8641b
anirudh loona <anirudhloona26@gmail.com>
2024-01-31 13:02:55 +0530

    initialized Q&A service

---
+++ qanda-service/sem8/src/main/java/com/major/sem8/QandaServiceApplication.java
@@ -0,0 +1,12 @@
+package com.major.sem8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class QandaServiceApplication {
+
+    public static void main(String[] args) {
+            SpringApplication.run(QandaServiceApplication.class, args);
+        }
+
+}
