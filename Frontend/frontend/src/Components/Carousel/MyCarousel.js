0d29b5d31c43bed1e5bc313b86f0066b9313c0eb
anirudh loona <anirudhloona26@gmail.com>
2024-02-11 01:31:40 +0530

    added carousel

---
+++ Frontend/frontend/src/Components/Carousel/MyCarousel.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import './MyCarousel.css'
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+const homeImages = [
+    {
+        placeName : "Taj Mahal",
+        image : "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1d/Taj_Mahal_%28Edited%29.jpeg/2560px-Taj_Mahal_%28Edited%29.jpeg"
+    },
+    {
+        placeName : "Taj Mahal",
+        image : "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1d/Taj_Mahal_%28Edited%29.jpeg/2560px-Taj_Mahal_%28Edited%29.jpeg"
+    },
+    {
+        placeName : "Taj Mahal",
+        image : "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1d/Taj_Mahal_%28Edited%29.jpeg/2560px-Taj_Mahal_%28Edited%29.jpeg"
+    },
+] 
+
+function MyCarousel() {
+    return (
+        <Carousel autoPlay={true} 
+            infiniteLoop={true} 
+            interval={3000} 
+            stopOnHover={true} 
+            dynamicHeight={false} 
+            width="100%" 
+            centerSlidePercentage={80} 
+            centerMode={true}
+            showThumbs={false}>
+            {homeImages?.map((item) => {
+                return(
+                    <div key={item.placeName} className='image'>
+                        <img alt='Not Available' src={item.image}/>
+                        <p className='placeName'>{item.placeName}</p>
+                    </div>
+                )
+            })}
+        </Carousel>
+    );
+}
+
+export default MyCarousel
--- Frontend/frontend/src/Components/Home/Home.js
+++ Frontend/frontend/src/Components/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Home.css'
 import Navbar from '../Navbar/Navbar'
+import Carousel from '../Carousel/MyCarousel'
 
 function Home() {
   return (
     <div className='homeContainer'>
       <Navbar/>
+      <Carousel/>
     </div>
   )
 }
