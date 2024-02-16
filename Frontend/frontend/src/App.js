301eaae7b8237c87fc726a50e87f05408fa85574
anirudh loona <anirudhloona26@gmail.com>
2024-02-16 16:33:23 +0530

    added default error page

--- Frontend/frontend/src/App.js
+++ Frontend/frontend/src/App.js
@@ -5,6 +5,7 @@ import Home from './Components/Home/Home';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from './Components/Navbar/Navbar';
 import Place from './Components/Place/Place';
+import ErrorPage from './Components/ErrorPage/ErrorPage';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path='/Home' element={<Home/>}/>
           <Route path='/explore/places/:city' element={<Explore/>}/>
           <Route path='/:city/place/:placeName' element={<Place/>}/>
+          <Route path='*' element={<ErrorPage/>}/>
         </Routes>
       </BrowserRouter>
       <Footer/>
