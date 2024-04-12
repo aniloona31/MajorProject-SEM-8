06281ae103d322f531ce7ff1345a915d71c802e7
anirudh loona <anirudhloona26@gmail.com>
2024-04-13 01:24:11 +0530

    q/a forum completed

--- Frontend/frontend/src/Components/Forum/Forum.js
+++ Frontend/frontend/src/Components/Forum/Forum.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Forum.css'
 import Question from '../Question/Question';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
@@ -16,6 +16,27 @@ function Forum() {
     const navigate = useNavigate();
     const [loader, setLoader] = useState(false);
 
+    useEffect(() => {
+        setLoader(true);
+        const url = process.env.REACT_APP_ROOT_URL + '/question/all'
+        const token = localStorage.getItem('token');
+        if(token == null)navigate('/sign-in')
+        axios.get(url, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+                "Content-Type": "application/json"
+            }
+        }).then((res) => {
+            if (res.status == 200) {
+                setQuestions(res.data);
+                setLoader(false);
+            }
+        }).catch((err) => {
+            toast.error('error while getting questions');
+            setLoader(false);
+            // navigate('/sign-i')
+        })
+    }, [])
 
     const handleChange = (e) => {
         setQuestion(e.target.value);
@@ -40,6 +61,9 @@ function Forum() {
             console.log(res)
             setLoader(false);
             setViewForm(false);
+            if(res.status == 200){
+                setQuestions((prev) => [...prev,res.data]);
+            }
         }).catch((err) => {
             console.log(err)
             setLoader(false);
@@ -59,38 +83,48 @@ function Forum() {
             {viewForm ?
                 <div className='questionScreen'>
                     {loader == false ?
-                    <>
-                    <div className='closeButton' onClick={() => { setViewForm((prev) => !prev) }}>
-                        <FontAwesomeIcon style={{ height: "50px", width: "50px", position: "absolute", top: "20px", right: "40px", color: "white", cursor: "pointer" }} icon={faXmark} />
-                    </div>
-                    <div className='questionForm'>
-                        <h3>Please Enter Your Question</h3>
-                        <textarea onChange={(e) => handleChange(e)} placeholder='Enter Here'></textarea>
-                        <button onClick={() => postQuestion()}>Submit</button>
-                    </div>
-                    </>
-                    :<Oval
-                        visible={true}
-                        height="80"
-                        width="80"
-                        color="#4fa94d"
-                        ariaLabel="oval-loading"
-                        wrapperStyle={{}}
-                        wrapperClass=""
-                    />}
+                        <>
+                            <div className='closeButton' onClick={() => { setViewForm((prev) => !prev) }}>
+                                <FontAwesomeIcon style={{ height: "50px", width: "50px", position: "absolute", top: "20px", right: "40px", color: "white", cursor: "pointer" }} icon={faXmark} />
+                            </div>
+                            <div className='questionForm'>
+                                <h3>Please Enter Your Question</h3>
+                                <textarea onChange={(e) => handleChange(e)} placeholder='Enter Here'></textarea>
+                                <button onClick={() => postQuestion()}>Submit</button>
+                            </div>
+                        </>
+                        : <Oval
+                            visible={true}
+                            height="80"
+                            width="80"
+                            color="#4fa94d"
+                            ariaLabel="oval-loading"
+                            wrapperStyle={{}}
+                            wrapperClass=""
+                        />}
                 </div>
                 :
                 <>
                     <div className='addQuestion'>
                         <button onClick={() => { setViewForm((prev) => !prev) }}>Ask Question</button>
                     </div>
-                    <div className='questions'>
-                        {questions?.map((question) => {
-                            return (
-                                <Question key={question.id} question={question} />
-                            )
-                        })}
-                    </div>
+                    {loader ?
+                        <Oval
+                            visible={true}
+                            height="80"
+                            width="80"
+                            color="#4fa94d"
+                            ariaLabel="oval-loading"
+                            wrapperStyle={{}}
+                            wrapperClass=""
+                        /> :
+                        <div className='questions'>
+                            {questions?.map((question) => {
+                                return (
+                                    <Question key={question.id} question={question} />
+                                )
+                            })}
+                        </div>}
                 </>
             }
         </div>
--- Frontend/frontend/src/Components/Question/Question.js
+++ Frontend/frontend/src/Components/Question/Question.js
@@ -1,10 +1,99 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './Question.css'
+import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCommentDots } from '@fortawesome/free-solid-svg-icons';
+import { useNavigate } from 'react-router-dom';
 
-function Question({question}) {
-  return (
-    <div>Question</div>
-  )
+function Question({ question }) {
+    const [answers, setAnsers] = useState([]);
+    const [answer, setAnswer] = useState('');
+    const navigate = useNavigate();
+    const [isOpen, setIsOpen] = useState(false);
+
+    useEffect(() => {
+        const url = process.env.REACT_APP_ROOT_URL + '/answer/get';
+        axios.get(url, {
+            headers: {
+                "Content-Type": "application/json"
+            },
+            params: {
+                "questionId": question.questionId
+            }
+        }).then((res) => {
+            if (res.status == 200) {
+                setAnsers(res.data);
+            }
+        }).catch((err) => {
+            toast.error('error while getting answers');
+        })
+    }, [])
+
+    const addAnswer = () => {
+        const token = localStorage.getItem('token');
+        const url = process.env.REACT_APP_ROOT_URL + '/answer/post'
+        if (token == null) navigate('/sign-in')
+        axios.post(url, {
+            "questionId": question.questionId,
+            "answer": answer,
+            "email": localStorage.getItem('email')
+        }, {
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+            }
+        }).then((res) => {
+            if (res.status == 200) {
+                setAnsers((prev) => [...prev, res.data]);
+                setAnswer('');
+            }
+        }).catch((err) => {
+            if(err.response.data.message == "un authorized access to application"){
+                navigate('/sign-in');
+            }
+            toast.error('error while adding answer')
+        })
+    }
+
+    const openAccordian = () => {
+        setIsOpen((prev) => !prev)
+    }
+
+    return (
+        <div className="accordion">
+            <ToastContainer />
+            <div className={`accordion-item ${isOpen ? 'open' : ''}`}>
+                <div className="accordion-item-header">
+                    <span className="accordion-item-header-title">{question.question}</span>
+                </div>
+                <div className='accordianButtons'>
+                    <span onClick={() => openAccordian()} style={{ display: "flex", alignItems: "center", flexDirection: "row", cursor: "pointer", color: "rgb(7,7,173)" }}><FontAwesomeIcon style={{ height: "20px", width: "20px", marginRight: "3px" }} icon={faCommentDots} />Replies</span>
+                    <input value={answer} onChange={(e) => setAnswer(e.target.value)} type='text' placeholder='Enter Here' />
+                    <button onClick={() => addAnswer()}>Submit</button>
+                </div>
+                {(answers.length > 0) ?
+                    answers.map((answer) => {
+                        return (
+                            <div className="accordion-item-description-wrapper">
+                                <div className="accordion-item-description">
+                                    <hr />
+                                    <div style={{padding:"10px 10px 10px 10px"}}>{answer.answer}</div>
+                                    <span style={{fontSize:"small",display:"flex",flexDirection:"row",paddingLeft:"10px",paddingRight:"10px", color:"grey",justifyContent:"space-between"}}><div>{answer.email}</div><div>{answer.date.substring(0,10)}</div></span>
+                                </div>
+                            </div>
+                        )
+                    }) :
+                    <div className="accordion-item-description-wrapper">
+                        <div className="accordion-item-description">
+                            <hr/>
+                            <p>No Replies Yet.</p>
+                        </div>
+                    </div>
+                }
+            </div>
+        </div>
+    )
 }
 
 export default Question
