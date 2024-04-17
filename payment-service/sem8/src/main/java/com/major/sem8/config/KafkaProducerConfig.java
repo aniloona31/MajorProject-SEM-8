a278709721c3eec2e8b366d991e152208450f10b
anirudh loona <anirudhloona26@gmail.com>
2024-04-18 01:04:03 +0530

    changed kafka config for payment-service and created docker image

--- payment-service/sem8/src/main/java/com/major/sem8/config/KafkaProducerConfig.java
+++ payment-service/sem8/src/main/java/com/major/sem8/config/KafkaProducerConfig.java
@@ -18,27 +18,27 @@ import java.util.Map;
 @Configuration
 public class KafkaProducerConfig {
 
-    @Bean
-    public Map<String,Object> producerConfig(){
-        Map<String,Object> props=new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                "localhost:9092");
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
-                StringSerializer.class);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
-                JsonSerializer.class);
-        return props;
-    }
-
-    @Bean
-    public ProducerFactory<String, Payment> producerFactory(){
-        return new DefaultKafkaProducerFactory<>(producerConfig());
-    }
-
-    @Bean
-    public KafkaTemplate<String,Payment> kafkaTemplate(){
-        return new KafkaTemplate<>(producerFactory());
-    }
+//    @Bean
+//    public Map<String,Object> producerConfig(){
+//        Map<String,Object> props=new HashMap<>();
+//        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+//                "localhost:9092");
+//        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+//                StringSerializer.class);
+//        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+//                JsonSerializer.class);
+//        return props;
+//    }
+//
+//    @Bean
+//    public ProducerFactory<String, Payment> producerFactory(){
+//        return new DefaultKafkaProducerFactory<>(producerConfig());
+//    }
+//
+//    @Bean
+//    public KafkaTemplate<String,Payment> kafkaTemplate(){
+//        return new KafkaTemplate<>(producerFactory());
+//    }
 
     @Bean
     public NewTopic topicExample() {
