8d57efd9c95404ec71a18730561cecf5c2f80d66
anirudh loona <anirudhloona26@gmail.com>
2024-04-18 01:42:11 +0530

    created image for email service

--- email-service/sem8/src/main/java/com/major/sem8/config/KafkaConsumerConfig.java
+++ email-service/sem8/src/main/java/com/major/sem8/config/KafkaConsumerConfig.java
@@ -17,29 +17,29 @@ import java.util.Map;
 @Configuration
 public class KafkaConsumerConfig {
 
-    @Bean
-    public Map<String, Object> consumerConfig() {
-        Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                "localhost:9092");
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
-                StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
-                JsonDeserializer.class);
-        props.put(JsonDeserializer.TRUSTED_PACKAGES, "com.major.sem8.entity");
-        return props;
-    }
-
-    @Bean
-    public ConsumerFactory<String,Object> consumerFactory(){
-        return new DefaultKafkaConsumerFactory<>(consumerConfig());
-    }
-
-    @Bean
-    public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, Object>> kafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, Object> factory =
-                new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(consumerFactory());
-        return factory;
-    }
+//    @Bean
+//    public Map<String, Object> consumerConfig() {
+//        Map<String, Object> props = new HashMap<>();
+//        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+//                "localhost:9092");
+//        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+//                StringDeserializer.class);
+//        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+//                JsonDeserializer.class);
+//        props.put(JsonDeserializer.TRUSTED_PACKAGES, "com.major.sem8.entity");
+//        return props;
+//    }
+//
+//    @Bean
+//    public ConsumerFactory<String,Object> consumerFactory(){
+//        return new DefaultKafkaConsumerFactory<>(consumerConfig());
+//    }
+//
+//    @Bean
+//    public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, Object>> kafkaListenerContainerFactory() {
+//        ConcurrentKafkaListenerContainerFactory<String, Object> factory =
+//                new ConcurrentKafkaListenerContainerFactory<>();
+//        factory.setConsumerFactory(consumerFactory());
+//        return factory;
+//    }
 }
